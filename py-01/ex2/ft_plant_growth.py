e78108799dea72de0446e3b2e71cd4802ff12823
Micael Jerry <fidimalala.mj@gmail.com>
2026-05-01 19:20:06 +0300

    PY-01 ex2

---
+++ py-01/ex2/ft_plant_growth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+class Plant:
+    def __init__(self, name: str, height: int, age: int):
+        self.__name: str = name
+        self.__height: float = float(height)
+        self.__age: int = age
+        self.__growth_per_day: float = round(self.__height / self.__age, 1)
+
+    def __str__(self) -> str:
+        return f"{self.__name}: {self.__height}cm, {self.__age} days old"
+
+    def grow(self) -> None:
+        self.__height = round(self.__height + self.__growth_per_day, 1)
+
+    def age(self) -> None:
+        self.__age += 1
+
+    def growth_simulation(self, num_of_days: int) -> None:
+        plant_cp: Plant = Plant(self.__name, self.__height, self.__age)
+
+        print("=== Garden Plant Growth ===")
+
+        for day in range(num_of_days):
+            print(f"=== Day {day + 1} ===")
+            plant_cp.grow()
+            plant_cp.age()
+            print(f"{plant_cp.__name}: {plant_cp.__height}cm, {plant_cp.__age} days old")
+
+        print(f"Growth this week: {round(plant_cp.__height - self.__height)}cm")
+
+
+def main() -> None:
+    rose: Plant = Plant("Rose", 25, 30)
+    rose.growth_simulation(7)
+
+
+if __name__ == "__main__":
+    main()
